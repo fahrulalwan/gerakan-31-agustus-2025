342371e1c90e50c310ee4877e2394e326c261f70
fahrulalwan <fahrulalwan@gmail.com>
2025-09-01 16:25:03 +0700

    feat: add Vercel Analytics to track website usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 
@@ -27,6 +28,7 @@ const RootLayout = ({
   <html lang="id">
     <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
       <AppContextProvider>{children}</AppContextProvider>
+      <Analytics />
     </body>
   </html>
 )
