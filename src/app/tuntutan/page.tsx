17cf834de7a7a7ab2b41e7ea91db9b6bfa1c5d59
Faishal Pasa <mfaishalpasa@gmail.com>
2025-09-02 14:50:03 +0700

    fix: update paragraph styling for improved readability on Tuntutan page

--- src/app/tuntutan/page.tsx
+++ src/app/tuntutan/page.tsx
@@ -96,7 +96,7 @@ const TuntutanPage = async ({
                   17+8 Tuntutan Rakyat
                   <span className="block text-[#EB8FBD]">#ResetIndonesia</span>
                 </h1>
-                <p className="lg:text-xl text-white mb-8 max-w-3xl mx-auto leading-5">
+                <p className="lg:text-xl text-white mb-8 max-w-3xl mx-auto leading-relaxed">
                   Daftar lengkap tuntutan yang harus dipenuhi untuk mewujudkan
                   Indonesia yang lebih adil, demokratis, dan sejahtera bagi
                   seluruh rakyat
